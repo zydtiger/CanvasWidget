11c01223c3e6eebfd6db7ef2611bd512064e7a2e
tigerding <zhiyuanding01@gmail.com>
2025-11-27 12:51:38 -0800

    Initial Commit

---
+++ CanvasWidget/CanvasWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasWidgetApp.swift
+//  CanvasWidget
+//
+//  Created by Zhiyuan Ding on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CanvasWidget
+//
+//  Created by Zhiyuan Ding on 11/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
