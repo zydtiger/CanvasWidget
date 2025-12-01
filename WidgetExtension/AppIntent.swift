4bba687871415a12b0221671a008c30aa261cc9c
tigerding <zhiyuanding01@gmail.com>
2025-12-01 15:22:55 -0500

    add widget extension

---
+++ WidgetExtension/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  WidgetExtension
+//
+//  Created by Zhiyuan Ding on 11/30/25.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ WidgetExtension/WidgetExtensionBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetExtensionBundle.swift
+//  WidgetExtension
+//
+//  Created by Zhiyuan Ding on 11/30/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        WidgetExtension()
+        WidgetExtensionControl()
+    }
+}
